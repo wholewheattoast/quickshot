41ff9bc77154fa9fbb6530c739aca7a50b491a8d
Shawn Eisenach <shawn@wholewheattoast.com>
2019-06-21 21:52:32 -0700

    More setup for distribution

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
-from distutils.core import setup
+import setuptools
 
-setup(
+setuptools.setup(
     name='Quickshot',
     version='0.1',
-    packages=['quickshot'],
+    author="Shawn Eisenach",
+    author_email="shawn@wholewheattoast.com",
+    packages=setuptools.find_packages(),
+    description="A simple tool to visdiff two web pages.",
     long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
 )
