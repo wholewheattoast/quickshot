7cae52c042613330ed1dee414bfc9a77846edd45
Shawn Eisenach <shawn@wholewheattoast.com>
2019-08-28 14:26:24 -0700

    Forgot to bump the version number when stamping release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='Quickshot',
-    version='0.1',
+    version='1.1',
     author="Shawn Eisenach",
     author_email="shawn@wholewheattoast.com",
     packages=setuptools.find_packages(),
